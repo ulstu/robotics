aaeaed8a7ec02dcfa596dcc40fbca4253fdb3cec
Kirill Svyatov <k.svyatov@ulstu.ru>
2021-03-04 10:39:52 +0400

    samples updated

--- practice/2020/samples/ex_01_turtlesim/src/start_turtle.py
+++ practice/2020/samples/ex_01_turtlesim/src/start_turtle.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import sys
 from geometry_msgs.msg import Twist
 
+print(sys.version)
+
+
 class GoForward():
     def __init__(self):
         rospy.init_node('turtlesim_script', anonymous=False)
-    	rospy.loginfo("To stop Turtle CTRL + C")
+        rospy.loginfo("To stop Turtle CTRL + C")
 
         rospy.on_shutdown(self.shutdown)
         self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-     
+
         r = rospy.Rate(1)
         i = 1
         while not rospy.is_shutdown():
@@ -22,21 +25,21 @@ class GoForward():
             move_cmd.linear.y = 0
             move_cmd.angular.z = 10 / i
             self.cmd_vel.publish(move_cmd)
-            r.sleep() 
-            i += 1                       
-        
+            r.sleep()
+            i += 1
+
     def shutdown(self):
         # stop turtlebot
         rospy.loginfo("Stop TurtleSim")
-	    # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
         self.cmd_vel.publish(Twist())
-	    # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
- 
+
+
 if __name__ == '__main__':
     try:
         GoForward()
     except:
         rospy.loginfo("GoForward node terminated.")
         rospy.loginfo(sys.exc_info()[:2])
-
